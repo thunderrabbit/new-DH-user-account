6714df064099522706c5a4e3aa7ee5a690da6432
Rob Nugen <ip.robnugen.com@robnugen.com>
2016-09-12 02:13:37 -0700

    setup.sh now sets up git the way I like

--- setup.sh
+++ setup.sh
@@ -3,3 +3,8 @@
 # This script is designed to be run on a newly created Dreamhost shell account
 
 DOMAIN=$(ls ~ | grep "\.")
+
+# first, set up git with my info
+git config --global core.excludesfile ~/.gitignore
+git config --global user.name "Rob Nugen"
+git config --global user.email $DOMAIN@robnugen.com
