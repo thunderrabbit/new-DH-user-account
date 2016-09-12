3e0224c9e1e253eef15ad8a8bd1da834fd9f6a44
Rob Nugen <ip.robnugen.com@robnugen.com>
2016-09-12 02:35:54 -0700

    Set up for remote login

--- setup.sh
+++ setup.sh
@@ -22,3 +22,11 @@ ls ~ | grep "\." >> $HOME/.gitignore
 
 # append the contents of .bash_profile_append to ~/.bash_profile
 cat .bash_profile_append >> $HOME/.bash_profile
+
+# Set up .ssh so we can login without using a password
+# N.B. this assumes we have copied our public key to the DH machine: ~/authorized_keys
+#      and further assumes there is no .ssh/ at all
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+mv ~/authorized_keys ~/.ssh/
+chmod 400 ~/.ssh/authorized_keys
