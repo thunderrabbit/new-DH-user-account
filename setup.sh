318b331df3fb778cc0394d5d305f97a9e98eec82
Rob Nugen <ip.robnugen.com@robnugen.com>
2016-09-12 02:20:31 -0700

    Add this domain's directory path to .gitignore, because we assume it will be under its own version control.

--- setup.sh
+++ setup.sh
@@ -12,3 +12,10 @@ git config --global user.email $DOMAIN@robnugen.com
 # copy files from git repo to ~
 cp .bash_aliases $HOME/
 cp .gitignore $HOME/
+
+# Now append this account's domain to the .gitignore
+# N.B. this depends on the domain name having dots.in.it.com
+#    and nothing else having dots (except for dot files such as .gitignore)
+
+## todo: figure out how to use $DOMAIN env variable
+ls ~ | grep "\." >> $HOME/.gitignore
